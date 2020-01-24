d94b76a41897ff246de490824c6dc907337b301f
shohag2003 <60250894+shohag2003@users.noreply.github.com>
2020-01-24 14:44:44 +0600

    Main.java

---
+++ Main.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sohag=new Scanner(System.in);
+		int nam,Nam1,Nam2,Nam3,a;
+
+		System.out.println("            welcome to hacking tool");
+
+
+		System.out.println("               ~by shohag khan");
+
+		System.out.println("{1}fb hack~");
+		System.out.println("{2}web hack~");
+		System.out.println("{3}likee hack~");
+		System.out.println("{4}password attack~");
+
+		System.out.println("{5}calculetor~");
+
+
+
+		System.out.println("");
+		nam=sohag.nextInt();
+		switch(nam){
+
+			case 1:
+				System.out.println("      {FB HACK}");
+				System.out.println("inter id:");
+				Nam2=sohag.nextInt();
+				for( a=1;a<=100000;a++){
+					System.out.println("********");
+				}
+
+				System.out.println("^attack seacesful^");
+
+				System.out.println("show password:run{3030}");
+				int s=sohag.nextInt();
+				if(s==3030){
+					System.out.println("Show Password:016sh44");
+
+				}else{
+					System.out.println("^password not found^");
+				}
+				break;
+				
+		case 5:
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		int b;
+	
+	
+
+				System.out.println(" inter any fast nambar");
+
+				s=sohag.nextInt();
+				System.out.println(" inter any last nambar");
+
+				b=sohag.nextInt();
+				s=s+b;
+				System.out.println(s);
+				s=s-b;
+				System.out.println(s);
+				s=s*b;
+				System.out.println(s);
+				s=s/b;
+				System.out.println(s);
+			
+		}
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		{
+		
+		
+		
+		
+		
+		
+		
+
+
+	}
+}
